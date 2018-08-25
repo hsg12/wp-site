e67f14c3df6143ebcc364c0ab61fd631abb40d06
php-user1 <test@test.com>
2018-08-25 15:01:39 -0400

    Manage and Create Custom Columns

--- wpsite/inc/custom-post-type.php
+++ wpsite/inc/custom-post-type.php
@@ -4,6 +4,9 @@ $activateContactForm = get_option( 'activate_contact_form' );
 
 if (isset($activateContactForm) && $activateContactForm == 1) {
   add_action( 'init', 'sunset_contact_custom_post_type' );
+
+  add_filter( 'manage_sunset-contact_posts_columns', 'sunset_set_contact_columns' );
+  add_action( 'manage_sunset-contact_posts_custom_column', 'sunset_contact_custom_column', 10, 2 );
 }
 
 function sunset_contact_custom_post_type() {
@@ -26,4 +29,27 @@ function sunset_contact_custom_post_type() {
   );
 
   register_post_type( 'sunset-contact', $args );
+}
+
+function sunset_set_contact_columns( $columns ) {
+  // unset( $columns['author'] );
+
+  $newColumns = array();
+  $newColumns['title']   = 'Full Name';
+  $newColumns['message'] = 'Message';
+  $newColumns['email']   = 'Email';
+  $newColumns['date']    = 'Date';
+  return $newColumns;
+}
+
+function sunset_contact_custom_column( $column, $post_id ) {
+  switch ( $column ) {
+    case 'message':
+      echo get_the_excerpt();
+      break;
+
+    case 'email':
+      echo 'Message information';
+      break;
+  }
 }
