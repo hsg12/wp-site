e90a4d5ccc5ab32ea703e78eda1be81acb24ce0f
php-user1 <test@test.com>
2018-09-05 15:08:00 -0400

    Code the Standard Post Format

--- wpsite/inc/theme-support.php
+++ wpsite/inc/theme-support.php
@@ -25,6 +25,8 @@ if (isset($customBackground) && $customBackground == 1) {
   add_theme_support( 'custom-background' );
 }
 
+add_theme_support( 'post-thumbnails' );
+
 /* Activate Nav Menu Option */
 
 function sunset_register_nav_menu() {
@@ -32,3 +34,36 @@ function sunset_register_nav_menu() {
 }
 
 add_action( 'after_setup_theme', 'sunset_register_nav_menu' );
+
+/* Blog Loop Custom Functions */
+
+function sunset_posted_meta() {
+  $posted_on  = human_time_diff( get_the_time('U'), current_time('timestamp') );
+  $categories = get_the_category();
+  $separator  = ', ';
+  $cat_output = '';
+  $i = 1;
+
+  if ( !empty($categories) ) {
+    foreach ($categories as $category) {
+      if ( $i > 1 ) {
+        $cat_output .= $separator;
+      }
+
+      $cat_output .= '<a href="' . esc_url(get_category_link( $category->term_id )) . '" alt="' . esc_attr( 'View all posts in %s', $category->name ) . '">';
+      $cat_output .= esc_html( $category->name ); 
+      $cat_output .= '</a>';
+
+      $i++;
+    }
+  }
+
+  $output  = '<span class="posted-on">Posted <a href="' . esc_url( get_permalink() ) . '">' . $posted_on . '</a> ago</span> / ';
+  $output .= '<span class="posted-in">' . $cat_output . '</span>';
+  
+  return $output;
+}
+
+function sunset_posted_footer() {
+  return 'Tags list and comment link';
+}
