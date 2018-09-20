dcd7cc94f5f5b6b5f1f5031d736f1aab50aa2cdb
php-user1 <test@test.com>
2018-09-20 16:10:48 -0400

    Quote Post Format

---
+++ wpsite/template-parts/content-quote.php
@@ -0,0 +1,26 @@
+<?php
+/*
+  @package wp-site
+
+  Quote Post Format
+*/
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'sunset-format-quote' ); ?>>
+
+  <header class="entry-header text-center">
+    <div class="row">
+      <div class="col-sm-12 col-md-8 offset-md-2">
+        <h2 class="quote-content"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php echo get_the_content(); ?></a></h2>
+        <?php the_title( '<h2 class="quote-author">- ', ' -</h2>' ); ?>
+      </div>
+    </div>
+  </header>
+
+  
+  
+  <footer class="entry-footer">
+    <?php echo sunset_posted_footer(); ?>
+  </footer>
+
+</article>
